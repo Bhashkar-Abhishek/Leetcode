854a437fb5d42d06bbee0b616fe4b585b88059df
Bhashkar-Abhishek <126654468+Bhashkar-Abhishek@users.noreply.github.com>
2023-03-29 10:02:18 +0530

    march 27

--- validParenthesis.js
+++ validParenthesis.js
@@ -17,9 +17,3 @@ function isValid(s) {
     
     return stack.length === 0;
   }
-
-  console.log(isValid('()')); // Output: true
-console.log(isValid('()[]{}')); // Output: true
-console.log(isValid('(]')); // Output: false
-console.log(isValid('([)]')); // Output: false
-console.log(isValid('{[]}')); // Output: true
