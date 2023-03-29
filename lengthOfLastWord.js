ca6be0543ece4d2eae18e32030cecc746caba99e
Bhashkar-Abhishek <126654468+Bhashkar-Abhishek@users.noreply.github.com>
2023-03-29 10:00:56 +0530

    march 27

--- lengthOfLastWord.js
+++ lengthOfLastWord.js
@@ -14,8 +14,3 @@ function lengthOfLastWord(s) {
     
     return length;
   }
-
-  console.log(lengthOfLastWord('Hello World')); // Output: 5
-console.log(lengthOfLastWord('   fly me   to   the moon  ')); // Output: 4
-console.log(lengthOfLastWord('luffy is still joyboy')); // Output: 6
-console.log(lengthOfLastWord('  ')); // Output: 0
